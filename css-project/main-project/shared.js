2bafdec049d2ac9f494e51bda8c5b45d328c72f5
rathorebijendra <rathorebijendra1999@gmail.com>
2019-03-22 15:25:23 +0530

    Adding Base Project with responsive design

--- css-project/main-project/shared.js
+++ css-project/main-project/shared.js
@@ -1,46 +1,45 @@
-var backdrop = document.querySelector('.backdrop');
-console.log(backdrop);
-var modal = document.querySelector('.modal');
-var negativebutton = document.querySelector('.modal__action--negative');
-var selectPlanButtons = document.querySelectorAll('.plan button');
-var toggleButton  = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav');
-console.log(mobileNav);
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
-//console.dir(selectPlanButtons);
-for(var i=0; i<selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        //modal.style.display='block';
-        //backdrop.style.display='block';
-        //modal.className = 'open'; This will actually overwrite the complete class list
-        modal.classList.add('open');
-        backdrop.classList.add('open');
-    });
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
 }
 
-if(negativebutton) {
-    negativebutton.addEventListener('click', closeModal);  
-}
-
-backdrop.addEventListener('click', function() {
-    //mobileNav.style.display='none';
-    mobileNav.classList.remove('open');
-    closeModal();
+backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
 });
 
-function closeModal(){
-   // modal.style.display='none';
-    //backdrop.style.display='none';
-    backdrop.classList.remove('open');
-    
-    if(modal) {
-        modal.classList.remove('open');  
-    }
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
 }
 
-toggleButton.addEventListener('click', function(){
-    //mobileNav.style.display='block';
-    //backdrop.style.display='block';
-    mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
